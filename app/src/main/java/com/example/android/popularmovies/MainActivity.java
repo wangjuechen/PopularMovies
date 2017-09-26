06ddcd048eabde5caea56da0f71c5a1f535c7974
Juechen <wjc.zhw@gmail.com>
2017-09-26 22:43:22 +1300

    SplashPage

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.popularmovies;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.inputmethodservice.Keyboard;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,31 +15,38 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.support.v7.widget.SearchView;
 
 import java.lang.reflect.Field;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SearchResultFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        SearchResultFragment.OnFragmentInteractionListener {
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     public ViewPager mViewPager;
+
+    private FrameLayout mFrameLayout;
 
     @BindView(R.id.tabs)
     public TabLayout tabLayout;
@@ -53,13 +61,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private FavoriteListFragment mFavoriteListFragment = new FavoriteListFragment();
 
-    private SearchResultFragment mSearchResultFragment  = new SearchResultFragment();
+    private SearchResultFragment mSearchResultFragment = new SearchResultFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MainActivity.mContext = getApplicationContext();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nav_drawer);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
@@ -77,7 +85,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 ActionBar.LayoutParams.WRAP_CONTENT,
                 ActionBar.LayoutParams.MATCH_PARENT,
                 Gravity.CENTER);
-        abar.setCustomView(viewActionBar, params);abar.setDisplayShowCustomEnabled(true);
+        abar.setCustomView(viewActionBar, params);
+        abar.setDisplayShowCustomEnabled(true);
         abar.setDisplayShowTitleEnabled(false);
         abar.setDisplayHomeAsUpEnabled(false);
         abar.setHomeButtonEnabled(false);
@@ -97,6 +106,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
+        mFrameLayout = (FrameLayout) findViewById(R.id.search_page_container);
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
@@ -112,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if (id == R.id.nav_manage) {
 
-        }else if (id == R.id.rate_link) {
+        } else if (id == R.id.rate_link) {
 
         } else if (id == R.id.nav_about) {
 
@@ -171,7 +181,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Associate searchable configuration with the SearchView
         SearchManager searchManager =
                 (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
+        final SearchView searchView =
                 (SearchView) menu.findItem(R.id.search_button).getActionView();
         searchView.setSearchableInfo(
                 searchManager.getSearchableInfo(getComponentName()));
@@ -180,32 +190,65 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public boolean onQueryTextSubmit(String query) {
 
-                Bundle bundle = new Bundle();
 
-                bundle.putString(SearchResultFragment.ARG_QUERY_PARAM, query);
 
-                mSearchResultFragment.setArguments(bundle);
+                mSearchResultFragment = SearchResultFragment.newInstance(query);
 
-                //TODO: try to put search layout into old layout, rather than a new activity
+                searchView.clearFocus();
 
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, mSearchResultFragment).commit();
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.search_page_container, mSearchResultFragment).
+                        commit();
+                //TODO: here need to be fix,
+
+                mFrameLayout.setVisibility(View.VISIBLE);
 
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                return false;
+                return true;
             }
         });
 
-        final int textViewID = searchView.getContext().getResources().getIdentifier("android:id/search_src_text",null, null);
+        MenuItem searchButton = menu.findItem(R.id.search_button);
+
+        MenuItemCompat.setOnActionExpandListener(searchButton, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+
+                tabLayout.setVisibility(View.GONE);
+
+                mViewPager.setVisibility(View.INVISIBLE);
+
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+
+                tabLayout.setVisibility(View.VISIBLE);
+
+                mViewPager.setVisibility(View.VISIBLE);//This works, the location of recyclerView is saved before shifting to search page
+
+                mFrameLayout.setVisibility(View.INVISIBLE);
+
+                return true;
+            }
+        });
+
+        final int textViewID = searchView.getContext().
+                getResources().
+                getIdentifier("android:id/search_src_text", null, null);
         final AutoCompleteTextView searchTextView = (AutoCompleteTextView) searchView.findViewById(textViewID);
         try {
             Field mCursorDrawableRes = TextView.class.getDeclaredField("mCursorDrawableRes");
             mCursorDrawableRes.setAccessible(true);
-            mCursorDrawableRes.set(searchTextView, 0); //This sets the cursor resource ID to 0 or @null which will make it visible on white background
-        } catch (Exception e) {}
+            mCursorDrawableRes.set(searchTextView, 0);
+            //This sets the cursor resource ID to 0 or @null which will make it visible on white background
+        } catch (Exception e) {
+        }
 
         return true;
     }
@@ -220,29 +263,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onBackPressed() {
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onSearchRequested() {
-
-        handleIntent(getIntent());
-
-        return super.onSearchRequested();
     }
 
     public static Context getmContext() {
         return MainActivity.mContext;
     }
 
-    private void handleIntent(Intent intent){
+    private void handleIntent(Intent intent) {
 
-        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
 
             query = intent.getStringExtra(SearchManager.QUERY);
 
